2749acfe8acb07811e3b15f2efd23f5980549202
Edu <edu@user.email.com>
2012-05-04 02:44:46 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chumon::Application.config.secret_token = 'cb37704c16af5504f38c6ee812935e9b8021fb7fb50208739ba0ac426470bbf7cd4783c0dec72c447ad3e30b06d4a05d2a59032ccabb385f720702039bd73f6d'
